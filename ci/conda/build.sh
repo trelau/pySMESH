1f7704c6948fd0828e120a507a327fa0598b7053
frmdstryr <frmdstryr@protonmail.com>
2021-08-22 13:55:38 -0400

    conda: Set SMESH paths on linux

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -9,7 +9,9 @@ cd build
 
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE="Release" \
-    -DPTHREAD_INCLUDE_DIRS="$PREFIX"
+    -DPTHREAD_INCLUDE_DIRS="$PREFIX" \
+    -DSMESH_INCLUDE_PATH="$PREFIX/include/smesh" \
+    -DSMESH_LIB_PATH="$PREFIX/lib"
 
 ninja install
 
